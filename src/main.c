bc15f2a82805c9c23ccd505a267fd24f5b2652f2
mohchams <mohchams@student.42.fr>
2025-04-04 12:55:32 +0200

    debur du programmee Pipx

--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
 #include "pipex.h"
 
 int main(int ac, int *av)
+{
+    if (ac < 5)
+    {
+        ft_printf("Usage: ./pipex file1 cmd1 cmd2 file2\n");
+        write(2, "Usage: ./pipex file1 cmd1 cmd2 file2\n", 37);
+        exit(1);
+    }
+
+    ft_putchar('\n');
+    return (0);
+}
